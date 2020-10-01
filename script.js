bfa9664a2c1ede65033ea254dede87d94d6d79f4
Matthew Oballe <oballematt@gmail.com>
2020-10-01 17:18:21 -0500

    changing row id's to numbers to allow program to determine time against time-block. Created function to determine if current hour is greater than less than or equal to time block

--- script.js
+++ script.js
@@ -13,22 +13,40 @@ $(".saveBtn").on("click", function(){
 
 })
 
-$("#obj1 .description").val(localStorage.getItem("obj1"))
-$("#obj2 .description").val(localStorage.getItem("obj2"))
-$("#obj3 .description").val(localStorage.getItem("obj3"))
-$("#obj4 .description").val(localStorage.getItem("obj4"))
-$("#obj5 .description").val(localStorage.getItem("obj5"))
-$("#obj6 .description").val(localStorage.getItem("obj6"))
-$("#obj7 .description").val(localStorage.getItem("obj7"))
-$("#obj8 .description").val(localStorage.getItem("obj8"))
-$("#obj9 .description").val(localStorage.getItem("obj9"))
+$("#9 .description").val(localStorage.getItem("9"))
+$("#10 .description").val(localStorage.getItem("10"))
+$("#11 .description").val(localStorage.getItem("11"))
+$("#12 .description").val(localStorage.getItem("12"))
+$("#13 .description").val(localStorage.getItem("13"))
+$("#14 .description").val(localStorage.getItem("14"))
+$("#15 .description").val(localStorage.getItem("15"))
+$("#16 .description").val(localStorage.getItem("16"))
+$("#17 .description").val(localStorage.getItem("17"))
 
 
 function hourCheck(){
    var hour = moment().hour()
+    $(".time-block").each(function(){
+        var differentHour = parseInt($(this).attr("id"))
+        console.log(hour, differentHour)
+        if (hour === differentHour){
+            $(this).addClass("present")
+        }
+        else if (hour > differentHour){
+            $(this).addClass("past")
 
+        }
+
+        else{
+            $(this).addClass("future")
+        }
+
+
+        
+    })
+   
 }
-
+hourCheck()
 
 
 
